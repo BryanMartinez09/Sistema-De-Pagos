fad524b6403b3495b52612e3c515544337beb4a4
Marvin y 2 mas <Marviny2mas@gmail.com>
2025-02-23 14:23:48 -0600

    KAN-10 #in-progress Se inició el desarrollo de la funcionalidad y arreglo del detalle de pagos

--- sistema-pagos/resources/views/pagos/index.blade.php
+++ sistema-pagos/resources/views/pagos/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <h2>Lista de Pagos </h2>
+        <h2>PAGOS</h2>
         <table class="table">
             <thead>
                 <tr>
