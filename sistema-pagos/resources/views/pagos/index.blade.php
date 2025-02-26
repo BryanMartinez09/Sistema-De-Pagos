052dad7d706dc34589a580311a3f38474717514a
Marvin y 2 mas <Marviny2mas@gmail.com>
2025-02-26 15:27:23 -0600

    KAN-9 #in- Un nuevo H7

--- sistema-pagos/resources/views/pagos/index.blade.php
+++ sistema-pagos/resources/views/pagos/index.blade.php
@@ -7,6 +7,7 @@
         <h4>Aprobaciones</h4>
         <h5>Listados completos</h5>
         <h6>List</h6>
+        <h7>Hola mundos</h7>
         <table class="table">
             <thead>
                 <tr>
